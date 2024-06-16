434a08c30f466aeaa3694d4b6489a1ffa22f0cef
zainezq <zaineulabideen@outlook.com>
2024-06-16 21:59:45 +0100

    modified frontend to include home/dashboard page. Hard coded token (auth service needed)

---
+++ ilm-learn-frontend/src/app/home/loggedIn/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  standalone: true,
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent {
+
+}
