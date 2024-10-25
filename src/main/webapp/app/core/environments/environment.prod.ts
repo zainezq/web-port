6c677b6db7cbb12f38210b96fb0f8a22f6ae02df
zaine <zaineulabideen@outlook.com>
2024-10-25 18:57:08 +0100

    Initial empty commit

--- src/main/webapp/app/core/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://your-production-api-url.com' // Production API base URL
-};
--- src/main/webapp/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app-config/app.config';
-import { AppComponent } from './app/app-config/app.component';
-
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
