d04f3b133bc6df7672c307d4f858a0a200ec7972
zaine <zaineulabideen@outlook.com>
2025-01-01 11:41:32 +0000

    fixing mobile issues

--- src/app/entities/sidebar/sidebar.component.ts
+++ src/app/entities/sidebar/sidebar.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    NgOptimizedImage
-  ],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
   standalone: true,
-  styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  isSidebarVisible = false;
 
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
+  }
 }
