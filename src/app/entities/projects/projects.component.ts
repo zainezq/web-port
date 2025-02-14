ad19a3ae74fa6aad2fb34cf52d8b8ca83583f532
zaine <zaineulabideen@outlook.com>
2025-02-14 00:11:35 +0000

    redoing the project ui

--- src/app/entities/projects/projects.component.ts
+++ src/app/entities/projects/projects.component.ts
@@ -1,36 +1,45 @@
-import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
-import {GithubService} from '../../services/github.service';
-import {NgForOf, NgIf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import { NgForOf, NgIf } from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [
-    NgIf,
-    NgForOf
-  ],
+  imports: [NgIf, NgForOf, RouterLink, RouterLinkActive],
   templateUrl: './projects.component.html',
   standalone: true,
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent implements OnInit {
-  repositories: any[] = [];
-  visibleRepositories: any[] = [];
+  manualProjects: any[] = [
+    {
+      name: 'Database handler - CLI Interface Written in Python',
+      description: 'A project that handles database operations using a CLI interface written in Python.',
+    },
+    {
+      name: 'Project Beta',
+      description: 'A machine learning project using TensorFlow.',
+      link: 'https://example.com/project-beta'
+    },
+    {
+      name: 'Project Gamma',
+      description: 'A full-stack application with Node.js and React.',
+      link: 'https://example.com/project-gamma'
+    }
+  ];
+
+  githubRepositories: any[] = [];
+  visibleGithubRepos: any[] = []; // Paginated GitHub repositories
   currentPage: number = 1;
-  reposPerPage: number = 5;  // Limit number of repositories per page
+  reposPerPage: number = 5; // Limit number of GitHub repositories per page
 
+  constructor(private githubService: GithubService) {}
 
-
-  constructor(
-    private githubService: GithubService,
-    private el: ElementRef,
-    private renderer: Renderer2
-  ) {}
   ngOnInit(): void {
     this.githubService.fetchRepositories().subscribe({
       next: (data) => {
-        this.repositories = data;
-        this.updateVisibleRepositories();
-
+        this.githubRepositories = data;
+        this.updateVisibleGithubRepos();
       },
       error: (error) => {
         console.error('Error fetching repositories:', error);
@@ -40,23 +49,24 @@ export class ProjectsComponent implements OnInit {
       }
     });
   }
-  updateVisibleRepositories(): void {
+
+  updateVisibleGithubRepos(): void {
     const start = (this.currentPage - 1) * this.reposPerPage;
     const end = start + this.reposPerPage;
-    this.visibleRepositories = this.repositories.slice(start, end);
+    this.visibleGithubRepos = this.githubRepositories.slice(start, end);
   }
 
   nextPage(): void {
-    if (this.currentPage * this.reposPerPage < this.repositories.length) {
+    if (this.currentPage * this.reposPerPage < this.githubRepositories.length) {
       this.currentPage++;
-      this.updateVisibleRepositories();
+      this.updateVisibleGithubRepos();
     }
   }
 
   prevPage(): void {
     if (this.currentPage > 1) {
       this.currentPage--;
-      this.updateVisibleRepositories();
+      this.updateVisibleGithubRepos();
     }
   }
 }
