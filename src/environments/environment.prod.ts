3b988bdffad098e27c5445d681d17e8bd1f18178
Zaine  Qayyum <zaine@MacBookPro.Home>
2025-03-14 08:51:03 +0000

    Fix environment file replacement & build settings x9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
+// environment.prod.ts
 export const environment = {
   production: true,
   firebase: {
-    apiKey: (window as any).process.env.FIREBASE_API_KEY,
-    authDomain: (window as any).process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: (window as any).process.env.FIREBASE_PROJECT_ID,
-    storageBucket: (window as any).process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: (window as any).process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: (window as any).process.env.FIREBASE_APP_ID,
-    measurementId: (window as any).process.env.FIREBASE_MEASUREMENT_ID
-  }
+    apiKey: process.env.FIREBASE_API_KEY || 'default_prod_api_key',
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'default_prod_auth_domain',
+    projectId: process.env.FIREBASE_PROJECT_ID || 'default_prod_project_id',
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'default_prod_storage_bucket',
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || 'default_prod_messaging_sender_id',
+    appId: process.env.FIREBASE_APP_ID || 'default_prod_app_id',
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'default_prod_measurement_id',
+  },
 };
