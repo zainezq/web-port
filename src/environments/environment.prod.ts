f153f35c5e72dc15def0277426b616acbec81f02
Zaine  Qayyum <zaine@MacBookPro.Home>
2025-03-14 08:40:35 +0000

    Fix environment file replacement & build settings x8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: process.env['FIREBASE_API_KEY'],
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-    projectId: process.env['FIREBASE_PROJECT_ID'],
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-    appId: process.env['FIREBASE_APP_ID'],
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID']
+    apiKey: (window as any).process.env.FIREBASE_API_KEY,
+    authDomain: (window as any).process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: (window as any).process.env.FIREBASE_PROJECT_ID,
+    storageBucket: (window as any).process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: (window as any).process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: (window as any).process.env.FIREBASE_APP_ID,
+    measurementId: (window as any).process.env.FIREBASE_MEASUREMENT_ID
   }
 };
