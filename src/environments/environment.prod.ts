141e150e28ea69915d0d110ae0ed68430f0f5eed
Zaine  Qayyum <zaine@MacBookPro.Home>
2025-03-14 07:48:23 +0000

    Generate environment.ts in Vercel buildx2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "", // Placeholder, will be replaced by `create-env.js`
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: (window as any).process.env.VITE_FIREBASE_API_KEY,
+    authDomain: (window as any).process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: (window as any).process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: (window as any).process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: (window as any).process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: (window as any).process.env.VITE_FIREBASE_APP_ID,
+    measurementId: (window as any).process.env.VITE_FIREBASE_MEASUREMENT_ID
   }
 };
--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,10 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import {ConfigService} from './app/config.service';
-import {APP_BOOTSTRAP_LISTENER} from '@angular/core';
 
 inject();
 
-function loadConfig(configService: ConfigService) {
-  return () => {
-    return configService.loadConfig().then(() => {
-      console.log("✅ Firebase config loaded!");
-    }).catch(err => console.error("❌ Failed to load config:", err));
-  };
-}
+
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
@@ -29,12 +21,5 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withFetch()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    ConfigService,
-    {
-      provide: APP_BOOTSTRAP_LISTENER,
-      useFactory: loadConfig,
-      deps: [ConfigService],
-      multi: true
-    }
     ]
 }).catch((err) => console.error(err));
