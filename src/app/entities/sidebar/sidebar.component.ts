97aaaa30fef5a919c81852ebd9cdfa9cefb87956
zaine <zaineulabideen@outlook.com>
2025-05-24 16:37:45 +0100

    test cloudflare

--- src/app/entities/sidebar/sidebar.component.ts
+++ src/app/entities/sidebar/sidebar.component.ts
@@ -20,7 +20,7 @@ export class SidebarComponent implements OnInit, OnDestroy {
   showSidebar = true;  // Default to false for small screens
   isLargeScreen = window.innerWidth >= 768;
   private resizeListener: any;
-  lastUpdated: Date = new Date('2025-05-24T11:16:01.760Z');
+  lastUpdated: Date = new Date('2025-05-24T15:36:15.423Z');
 
   constructor(private router: Router) {}
 
